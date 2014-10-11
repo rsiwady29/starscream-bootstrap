8f877e495e50d179e9992627ab738cd292efb1f1
Richard Siwady <rsiwady29@gmail.com>
2014-10-11 09:19:08 -0600

    adding toastr, project name validations, starscream icon :)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,22 +10,34 @@
 angular.module('starscreamBootstrapApp')
   .controller('MainCtrl', ['$scope','baseUrlService', function ($scope, baseUrlService) {
 
+        $scope.projectname = '';
+
         $scope.getStarscream = function(){
 
-            if($scope.projectname == ''){
+            if($scope.projectname.trim() == ''){
+                toastr.error('You must enter a project name');
                 return;
             }
 
             if($scope.projectname.toLowerCase().indexOf('starscream') != -1){
                 // we currently don't support projects with starscream inside their name < :( >
+                toastr.error('Starscream is not a valid project name');
                 return;
             }
 
-            /// TODO: Project name should only contain letters
+            $scope.projectname = $scope.projectname.replace(' ', '');
 
-            var url = baseUrlService.GetBaseUrl();
-            var downloadLink = url + '/getStarscream?projectname='+$scope.projectname.trim().replace(' ','');
+            var regex = /^[a-zA-Z]$/;
 
-            window.location = downloadLink;
+            if(regex.test($scope.projectname)){
+                var url = baseUrlService.GetBaseUrl();
+                var downloadLink = url + '/getStarscream?projectname='+$scope.projectname.trim().replace(' ','');
+
+                window.location = downloadLink;
+                toastr.success('Happy Coding!');
+            }
+            else{
+                toastr.error('Invalid project name!');
+            }
         }
   }]);
